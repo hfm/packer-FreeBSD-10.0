68c1d6d34a6ea590c3475dbbe54117459f156472
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-02-08 05:33:43 +0900

    不要なコメントを削除

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-echo ====================================
-echo      virtualbox-ose-additions
-echo ====================================
 pkg install -y virtualbox-ose-additions
 
-echo ====================================
-echo         adding /etc/rc.conf
-echo ====================================
 cat <<'EOF' >> /etc/rc.conf
 
 # VirtualBox configuration
@@ -16,9 +10,6 @@ vboxnet_enable="YES"
 vboxservice_enable="YES"
 EOF
 
-echo ====================================
-echo      adding /boot/loader.conf
-echo ====================================
 cat <<'EOF' >> /boot/loader.conf
 vboxdrv_load="YES"
 EOF
