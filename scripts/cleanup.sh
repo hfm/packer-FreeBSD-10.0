5f35eff79390246ad76c291f0f4fc4024b26893f
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-04-27 19:54:44 +0900

    remove unnecessary files

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
+rm -f /home/vagrant/*.iso
 rm -rf /tmp/*
+rm -rf /tmp/chef*
+rm -rf /tmp/puppet*
+rm -rf /var/db/freebsd-update/files
+mkdir /var/db/freebsd-update/files
+rm -f /var/db/freebsd-update/*-rollback
+rm -rf /var/db/freebsd-update/install.*
+rm -rf /boot/kernel.old
+rm -rf /usr/src/*
+rm -f /*.core
+
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -rf /EMPTY
+sync
